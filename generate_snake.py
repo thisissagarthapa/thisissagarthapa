b3fa66346043be95c4d2ad1e2d672e4de02e0ebe
Sagar Thapa <139972637+thisissagarthapa@users.noreply.github.com>
2024-08-29 15:02:21 +0545

    Delete generate_snake.py

--- generate_snake.py
+++
@@ -1,9 +0,0 @@
-import os
-from platane_snk import generate_snake
-
-def main():
-    os.makedirs("dist", exist_ok=True)  # Ensure the 'dist' directory exists
-    generate_snake(github_user_name="thisissagarthapa", output_path="dist/github-contribution-grid-snake.svg")
-
-if __name__ == "__main__":
-    main()
